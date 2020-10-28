fb644084f0f0f18c36e9fc372287ed5283c8013e
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2020-10-28 10:52:01 +0100

    pierwszy commit

---
+++ src/CezarsCode.java
@@ -0,0 +1,24 @@
+public class CezarsCode {
+    public static void main(String[] args) {
+        String zdanie = "I ty, Brutusie, przeciwko mnie?";
+
+        System.out.println(zdanie);
+
+        char[] zdanie_array = zdanie.toCharArray();
+
+        for (int i=0; i<zdanie.length(); i++){
+            if(zdanie_array[i] >= 65 && zdanie_array[i] <= 87 || zdanie_array[i] >= 97 && zdanie_array[i] <= 119 ){
+                zdanie_array[i] += 3;
+            }
+            switch (zdanie_array[i]){
+                case 88: zdanie_array[i] = 'A';
+                case 89: zdanie_array[i] = 'B';
+                case 90: zdanie_array[i] = 'C';
+                case 120: zdanie_array[i] = 'a';
+                case 121: zdanie_array[i] = 'b';
+                case 122: zdanie_array[i] = 'c';
+            }
+        }
+        System.out.println(zdanie_array);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 12;
+        double f = 4.5;
+        float af = 5.6f;
+        String s = "hello ";
+        boolean b = true;
+        int[] array = {1,2,3};
+
+        final int c;
+        c = 19;
+
+//        c = 82;
+
+        double sum = a + f;
+        System.out.println("wynik:" + sum);
+
+    }
+}
