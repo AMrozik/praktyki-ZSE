66b674118df97ef55c61595a7dd0fafd23435df7
Kamil Skrzypkowski <sk.kamil6@wp.pl>
2020-11-02 16:16:10 +0100

    Comentary Update
    Dorzuciłem komentarze do kodu plus kilka linijek zminiło miejsce dla przejrzystości.

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,32 @@ public class Main {
         int a = 7;
         String s = new String("hello");
 
+        //Tworzymy dwa obiekty typu Auto
         Auto BMW = new Auto();
         Auto MERC = new Auto();
 
+        //Wypisywanie poszczególnych wartości z obiektów
         System.out.println(BMW.color);
         System.out.println(MERC.color);
         System.out.println(MERC.liczba_miejsc);
         System.out.println(MERC.velocity);
 
+        //Ustawianie wartości wewnątrz obiektów
         BMW.set_all("Biały", 5, 12, 220);
-
         MERC.set_all("Złoty", 10, 120, 2200);
 
-        System.out.println("Podaj color Bety");
-
+        //Tym możecie podać wartości do obiektów z konsoli
+        //Tworzymy obiekt skaner do wczytywania wartości
         Scanner scanner = new Scanner(System.in);
+
+        //Wyświetlamy użytkownikowi odpowiedni komunikat oraz pobieramy wartość wpisaną
+        System.out.println("Podaj color Bety");
         String color = scanner.next();
 
-
+        //Wpisujemy wartośc wprowadzoną do wartości przechowywanych przez obiekt
         BMW.set_all(color, 5, 12, 220);
 
+        //Sprawdzamy czy się udało
         System.out.println(BMW.color);
 
     }
